281e3c3cd630be6925d7de53f00a45b904c15644
Gökhan Özbulak <gokhanozbulak@gmail.com>
2023-09-23 17:21:26 +0200

    Rearrange system paths for tests for running from anywhere

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -2,12 +2,12 @@ import os
 import sys
 import unittest
 
-module_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
-sys.path.insert(0, module_path)
+test_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(test_path, '..'))
 
 def main():
     loader = unittest.TestLoader()
-    test_suite = loader.discover('opendv')
+    test_suite = loader.discover(os.path.join(test_path, 'opendv'))
     test_runner = unittest.TextTestRunner(verbosity=2)
     test_runner.run(test_suite)
 
